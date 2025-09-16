66e0d4b31ad7ed6765155b67fc0410896617f1d1
kaydenireland <kaydenireland@aol.com>
2025-09-16 03:01:15 -0400

    Initial Clean Up

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    orbit_lib::run()
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
